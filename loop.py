eb86a31fa98f42bfb9587366be07218903dedff0
Eric Fredricksen <efredricksen@gmail.com>
2020-03-18 23:02:13 -0700

    Allow mulitple command watch loops. This minorly complicates the code but it turns out to be much nicer that this \&\& stuff

--- loop.py
+++ loop.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python
 
 """Usage: loop.py OPTS COMMAND [-- WATCH...]
+       loop.py OPTS COMMAND [--WATCH...] ++ OPTS COMMAND [--WATCH...] ...
 
 Wait for changes to FILEs NAMEd on the command line, Run the COMMAND
 whenever one of them changes. (However, filenames following a '>' in
@@ -11,12 +12,20 @@ OPTS:
   -#        Run command through head -# (for some integer #)
   -i FNAME  Ignore changes to FNAME even if appears in COMMAND or WATCH
   -I        Ignore all command line names not explicitly in WATCH
-  -d        'Daemon' mode - run task in background and restart as needed
+  -d        'Daemon' mode - start task in background and restart as needed
   -q        Print less info
   -a        Always restart when command quits
-  -f        Fast mode - shorter sleep time before watching for changes
+  -f        Faster polling for changes (applies to all command watch loops)
+  -x        Run command once on startup without waiting for changes
 
-WATCH: Files listed after -- but are watched for changes
+WATCH: Files listed after -- are (only) watched for changes
+
+Multiple command watch loops can be specified by separating them with ++.
+
+Specifying any other command line options causes the output of the command
+to be piped through `head`, and the option is passed to `head`.
+
+Hitting the enter key causes all commands to run (and/or daemons to be restarted).
 
 EXAMPLES:
   loop.py gcc test.c
@@ -30,105 +39,136 @@ EXAMPLES:
     the io redirection operators must be escaped. Also note that
     though nightfile is mentioned in the command, it's after a '>' and
     therefore not watched.
+
+  loop.py swiftc -emit-library helper.swift ++ swiftc -lhelper program.swift -- libhelper.dylib ++ ./program
+    Recompile the library "helper" when "helper.swift" changes. And:
+    Recompile "program" when program.swift or the library changes. And:
+    Run the program whenever it is regenerated.
 """
 
 import os, sys, time, itertools, signal
 
+
+SLEEPTIME = 1
+
+
 def main():
-  HEAD = ''
-  IGNORE = set()
-  AUTOWATCH = True
-  BACKGROUND = False
-  QUIET = False
-  ALWAYS = False
-  SLEEPTIME = 1
-
-  args = sys.argv[1:]
-  while (args or [''])[0].startswith('-'):
-    opt = args.pop(0)
-    if not args:
-      usage()
-    if opt == '-i':
-      IGNORE.add(args.pop(0))
-    elif opt == '-I':
-      AUTOWATCH = False
-    elif opt == '-d':
-      BACKGROUND = True
-    elif opt == '-q':
-      QUIET = True
-    elif opt == '-a':
-      ALWAYS = True
-    elif opt == '-f':
-      SLEEPTIME /= 4
-    else:
-      HEAD = ' 2>&1 | head ' + opt
-
-  for i in range(len(args)):
-    if args[i][:1] == '@':
-      args[i] = args[i][1:]
-      IGNORE.add(args[i])
-
-  if not args:
-    usage()
-
-  # Split args into [COMMAND, WATCH]
-  cfi = [list(g) for k,g in itertools.groupby(args, lambda x: x != '--') if k]
-
-  command = cfi.pop(0)
-  filenames = [a for a,b in zip(command, [0] + command[:-1]) if b != '>']
-  filenames = set([f for f in filenames if os.path.exists(f) and AUTOWATCH])
-  filenames |= set(cfi and cfi.pop(0))
-  filenames -= IGNORE
-
-  if not BACKGROUND:
-    command = ' '.join(command)
-
-  pid = None
-  mtime = None
+  tasks = [list(g) for k,g in itertools.groupby(sys.argv[1:], lambda x: x != '++') if k]
+  tasks = map(lambda task: Task(task), tasks)
   while True:
-    m = [os.stat(filename).st_mtime for filename in filenames
-         if os.path.exists(filename)]
+    for task in tasks:
+      task.checkForChanges()
 
-    if not BACKGROUND:
-
-      if (mtime != m) or ALWAYS:
-        #os.system('clear')
-        print '$ ' + command
-        os.system(command + HEAD)
-        mtime = m
-        if not (QUIET or ALWAYS):
-          print "############################################################"
-          print "Watching:", ', '.join(filenames)
-
-      if enterKeyHasBeenHit():
-        mtime = None
-      else:
-        time.sleep(SLEEPTIME)
-
+    if enterKeyHasBeenHit():
+      for task in tasks:
+        task.mtime = None
     else:
-
-      if mtime != m:
-        pid = restart(pid, command)
-        mtime = m
-      else:
-        try:
-          os.kill(pid, 0)
-        except:
-          pid = restart(pid, command)
-
       try:
         time.sleep(SLEEPTIME)
       except KeyboardInterrupt:
-        print "\nKilling", pid, "^C again to quit"
-        if pid:
-          os.kill(pid, signal.SIGTERM)
-          pid = None
-        try:
-          time.sleep(1)
-        except KeyboardInterrupt:
-          print
-          break
+        killed = 0
+        for task in tasks:
+          if task.pid:
+            print "\nKilling", task.pid
+            os.kill(task.pid, signal.SIGTERM)
+            task.pid = None
+            killed += 1
+        if killed:
+          print "^C again to quit"
+          try:
+            time.sleep(1)
+          except KeyboardInterrupt:
+            print
+            sys.exit(0)
+        else:
+          sys.exit(0)
 
+
+class Task:
+  def __init__(self, args):
+    IGNORE = set()
+    AUTOWATCH = True
+    WAIT = True
+    self.HEAD = ''
+    self.BACKGROUND = False
+    self.QUIET = False
+    self.ALWAYS = False
+    global SLEEPTIME
+
+    while args and args[0].startswith('-'):
+      opt = args.pop(0)
+      if not args:
+        usage()
+      if opt == '-i':
+        IGNORE.add(args.pop(0))
+      elif opt == '-I':
+        AUTOWATCH = False
+      elif opt == '-d':
+        self.BACKGROUND = True
+      elif opt == '-q':
+        self.QUIET = True
+      elif opt == '-a':
+        self.ALWAYS = True
+      elif opt == '-f':
+        SLEEPTIME /= 4
+      elif opt == '-x':
+        WAIT = False
+      else:
+        self.HEAD = ' 2>&1 | head ' + opt
+
+    for i in range(len(args)):
+      if args[i][:1] == '@':
+        args[i] = args[i][1:]
+        IGNORE.add(args[i])
+
+    if not args:
+      usage()
+
+    # Split args into [COMMAND, WATCH]
+    cfi = [list(g) for k,g in itertools.groupby(args, lambda x: x != '--') if k]
+
+    self.command = cfi.pop(0)
+    filenames = [a for a,b in zip(self.command, [0] + self.command[:-1]) if b != '>']
+    filenames = set([f for f in filenames if os.path.exists(f) and AUTOWATCH])
+    filenames |= set(cfi and cfi.pop(0))
+    filenames -= IGNORE
+    self.filenames = filenames
+
+    if not self.BACKGROUND:
+      self.command = ' '.join(self.command)
+
+    self.pid = None
+    self.mtime = None
+    if WAIT and not self.BACKGROUND:
+      self.mtime = [os.stat(filename).st_mtime for filename in self.filenames
+                    if os.path.exists(filename)]
+
+  def checkForChanges(self):
+    m = [os.stat(filename).st_mtime for filename in self.filenames
+         if os.path.exists(filename)]
+    if self.ALWAYS:
+      m = None
+
+    if not self.BACKGROUND:
+
+      if self.mtime != m:
+        print '$ ' + self.command
+        os.system(self.command + self.HEAD)
+        self.mtime = m
+        if not (self.QUIET or self.ALWAYS):
+          print "############################################################"
+          print "Watching:", ', '.join(self.filenames)
+
+    else:
+
+      if self.mtime != m:
+        self.pid = restart(self.pid, self.command)
+        self.mtime = m
+      else:
+        try:
+          os.kill(self.pid, 0)  # look for the running process
+        except:
+          self.pid = restart(self.pid, self.command)
 
 
 def usage():
@@ -150,7 +190,7 @@ def restart(pid, command):
     print "Killing pid", pid
     os.kill(pid, signal.SIGTERM)
   pid = os.spawnlp(os.P_NOWAIT, command[0], *command)
-  print "Started", pid
+  print "Started pid", pid
   return pid
 
 
